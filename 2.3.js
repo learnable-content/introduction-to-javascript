16404129abba1d806d2389a192792e8ba7e75e6f
Angela Molina <angela.molina@learnable.com>
2015-06-15 16:57:40 +1000

    Added code sample

---
+++ 2.3.js
@@ -0,0 +1,18 @@
+console.log(1 + 2);
+// always returns 3
+
+var first;
+var second;
+
+first = 1;
+second = 2;
+
+console.log(first);
+// returns 1
+
+console.log(first + second);
+// returns 3
+
+first = 3;
+console.log(first + second);
+// returns 5
