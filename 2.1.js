4ae9fafd52149ab869d3761a14a321dbd3ca2414
Angela Molina <angela.molina@learnable.com>
2015-06-15 16:56:13 +1000

    Added code sample

---
+++ 2.1.js
@@ -0,0 +1,10 @@
+var example = "coffee";
+console.log(example);
+// this should return the string "coffee"
+
+typeof(example);
+// this should return the value "string"
+
+var first = 1;
+typeof(first);
+// this should return the value "number"
