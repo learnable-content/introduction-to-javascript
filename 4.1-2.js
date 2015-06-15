3dbf882eaa4ac128bdd565b0822dd7d42f1629bd
Angela Molina <angela.molina@learnable.com>
2015-06-15 17:05:08 +1000

    Added code sample

---
+++ 4.1-2.js
@@ -0,0 +1,17 @@
+var colors = ["red", "green", "blue"];
+var selection = 0;
+
+console.log("You chose " + colors[selection]);
+// "You chose red"
+
+// PSEUDO CODE (Not real JavaScript)
+IF THE SELECTION IS RED {
+  console.log("Just like Red Riding Hood")
+} OTHERWISE, IF THE SELECTION IS GREEN {
+  console.log("Just like Peter Pan")  
+} OTHERWISE, IF THE SELECTION IS BLUE {
+  console.log("Just like Little Boy Blue")
+} OTHERWISE {
+  console.log("That's nice")
+}
+// "Just like Red Riding Hood"
---
+++ 4.1.js
@@ -0,0 +1,14 @@
+var colors = ["red", "green", "blue"];
+var question;
+
+question = "Do you like " + colors[0] + "?";
+console.log(question);
+// "Do you like red?"
+
+question = "Do you like " + colors[1] + "?";
+console.log(question);
+// "Do you like green?"
+
+question = "Do you like " + colors[2] + "?";
+console.log(question);
+// "Do you like blue?"
