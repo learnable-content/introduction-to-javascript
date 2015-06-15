d86da33f1412abae1f28df4ebdbd72102c617ada
Angela Molina <angela.molina@learnable.com>
2015-06-15 16:56:50 +1000

    Added code sample

---
+++ 2.2.js
@@ -0,0 +1,16 @@
+var example = "coffee";
+console.log(example);
+// this should return the string "coffee"
+
+typeof(example);
+// this should return the value "string"
+
+var first = 1;
+typeof(first);
+// this should return the value "number"
+
+var 2nd = 2;
+// this name is invalid because it begins with a number
+
+var this = "thing";
+// this name is invalid because this is a reserved word
