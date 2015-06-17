3a6a033ec28706d9d86b62705b8d8786a4ecf50d
Angela Molina <angela.molina@learnable.com>
2015-06-17 11:47:19 +1000

    Updated code sample

--- 6.1-1.js
+++ 6.1-1.js
@@ -1,5 +1,5 @@
-var input = prompt("What is your name?");
+
 // will display a prompt in the browser
 // and set input to the value entered
-alert("Hello " + input);
+
 // will greet your user in the browser
--- 6.1.js
+++ 6.1.js
@@ -1,9 +1,5 @@
-var rand = function(max) {
-  return(Math.floor(Math.random() * max) + 1);
-};
 
-console.log(rand(5));
 // 2 (maybe)
-console.log(rand(5));
+
 // 1 (maybe)
 // etc.
