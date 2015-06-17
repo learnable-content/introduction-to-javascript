b26f53f41ba45408bdda5d8f4d466d2eb8b82757
Angela Molina <angela.molina@learnable.com>
2015-06-17 11:09:54 +1000

    Updated assets

--- 2.2.js
+++ 2.2.js
@@ -9,8 +9,8 @@ var first = 1;
 typeof(first);
 // this should return the value "number"
 
-var 2nd = 2;
+
 // this name is invalid because it begins with a number
 
-var this = "thing";
+
 // this name is invalid because this is a reserved word
