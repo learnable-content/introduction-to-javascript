9727f4eb7fc26de79baf8cc04d1df80964bdfd44
Angela Molina <angela.molina@learnable.com>
2015-06-17 11:44:28 +1000

    Updated code sample

--- 5.4-2.js
+++ 5.4-2.js
@@ -1,20 +1,3 @@
-function greeter(str, arr) {
-  var counter;
-  var phrase;
-  for (counter = 0; counter < arr.length; counter++) {
-     phrase = str + " " + arr[counter];
-     console.log(shout(phrase));
-  }
 
-  function shout(words, emphasis) {
-    var punctuation = emphasis;
-    if (!punctuation) {
-      punctuation = "!";
-    }
-    return(words.toUpperCase() + punctuation);
-  }
-}
-
-greeter("Hello", ["Fred", "Judy"]);
 // "HELLO FRED!"
 // "HELLO JUDY!"
--- 5.4.js
+++ 5.4.js
@@ -1,13 +1,6 @@
-function greeter(str, arr) {
-  var counter;
-  for (counter = 0; counter < arr.length; counter++) {
-    console.log(str + " " + arr[counter]);
-  }
-}
 
-console.log(window.greeter);
 // the code of the function greeter
 
-window.greeter("Hello", ["Fred", "Judy"]);
+
 // Hello Fred
 // Hello Judy
