6fca516f7a4aa3a43ca1fb8cadcf280768a9ca7b
Angela Molina <angela.molina@learnable.com>
2015-06-17 11:16:21 +1000

    Updated code sample

--- 2.6-2.js
+++ 2.6-2.js
@@ -1,6 +1,5 @@
-var notNumber = 5 * "Word";
-console.log(notNumber);
+
 // NaN
 
-console.log(typeof(notNumber));
+
 // "number"
--- 2.6.js
+++ 2.6.js
@@ -1,4 +1,2 @@
-var integer = 1;
-var float = 1.0;
-console.log(integer == float);
+
 // true
