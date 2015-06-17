f1803b3e804c98fc2ab9df155006083bca404b08
Angela Molina <angela.molina@learnable.com>
2015-06-17 11:34:20 +1000

    Updated code sample

--- 4.2.js
+++ 4.2.js
@@ -1,9 +1,5 @@
-var color = "red";
 
-console.log("You chose " + color);
 // "You chose red"
 
-if (color == "blue") {
-  console.log("Blue is my favorite color");
-} 
+
 // no output because (color == "blue") is false
