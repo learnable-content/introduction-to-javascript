876dff2b2aeeb0f1a65518fa32a60cf0e871bb4c
Angela Molina <angela.molina@learnable.com>
2015-06-17 11:08:52 +1000

    Updated assets

--- 2.1.js
+++ 2.1.js
@@ -1,10 +1,8 @@
-var example = "coffee";
-console.log(example);
+
 // this should return the string "coffee"
 
-typeof(example);
+
 // this should return the value "string"
 
-var first = 1;
-typeof(first);
+
 // this should return the value "number"
