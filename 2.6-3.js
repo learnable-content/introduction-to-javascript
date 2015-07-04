d73e116d4ef1bf160d98892b3ec867a07c17a3d9
M. David Green <mdavidgreen@users.noreply.github.com>
2015-07-04 14:59:32 -0700

    Update 2.6-3.js

--- 2.6-3.js
+++ 2.6-3.js
@@ -1,17 +1,9 @@
-
-// "5.5"
-
-
 // 5 (parsed using base 10)
-
 
 // 5.5
 
+// "5.5"
 
-// 6 (rounded up)
+// "6" (rounded up)
 
-
-// 5.50 (with a total length of 3 digits)
-
-
-// "5.500e+0" (with a precision of 3 decimals)
+// "5.50" (with a total length of 3 digits)
